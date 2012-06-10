32387b058fe5e16553d3640b32c03c6ebfc4138b
computermacgyver <computermacgyver@gmail.com>
2012-06-10 20:14:06 +0100

    reworked gui, added ability to close info pane, added fancybox, zoom now zooms from the location of the cursor! :-), implemented zoom & reset buttons

--- map/js/main.js
+++ map/js/main.js
@@ -1,4 +1,5 @@
-$(function() {
+//library=$(function() {
+$(document).ready(function() {
 	
 	// map
 	var canvas=$('#canvas');
@@ -48,13 +49,13 @@ $(function() {
 	};
 	
 	//var chart=Raphael(elem.attr('id'), elem.parent().width(), elem.parent().height()-(elem.position().top));
-	var chart=Raphael(elem.attr('id'), elem.parent().width(), elem.parent().height()-$('#chartname').outerHeight());
+	var chart=Raphael(elem.attr('id'), elem.parent().width(), elem.parent().height());//-$('#chartname').outerHeight()
 	var plot={};
 	plot.x=0;
 	plot.y=0;
 	plot.gutter=Math.round(chart.width*0.1);
 	plot.width=(chart.width-(plot.gutter*(chartlabels.length-1)))/chartlabels.length;
-	plot.height=chart.height;
+	plot.height=Math.min(chart.height,300);
 	
 	var labels=chart.set();
 	for (var i=0; i<chartlabels.length; i++) {
@@ -92,10 +93,24 @@ $(function() {
 	
 	// initialize
 	$(window).smartresize(resize);
-	$('#toolbar #reset').click(reset);
+
+	//buttons
+	$("#reset").click(reset);
+	$("#zoomIn").click(zoomOut);
+	$("#zoomOut").click(zoomIn);
+	$("#attributepane .left-close").click(function(){	
+		$("#attributepane").hide();		
+	});
+	//more info
+	$(".fb").fancybox({
+        minWidth: 400,
+        maxWidth: 800,
+        minHeight: 300,
+        maxHeight: 600
+    });
 	datachange('World', literacy['world']);
 	set.drag(move(set), movestart);
-	svg.mousewheel(zoom);
+	svg.mousewheel(zoom);    
 	
 	function paint(country) {
 		var obj=map.path(image.shapes[country]);
@@ -157,25 +172,127 @@ $(function() {
 	}
 
 	function zoom(e, delta) {
-		e.preventDefault();
-		var width, height;
+		var width, height,factor,x,y;
 		width=viewbox.width;
 		height=viewbox.height;
-		var factor;
-		if (delta<0) factor=0.95;
-		else factor=1.05;
+
+		centerX=viewbox.width/2+viewbox.x;
+		centerY=viewbox.height/2+viewbox.y;
+		
+		if (e!=null) {
+			e.preventDefault();
+			coords=screen2svgCoords(e.clientX,e.clientY);
+		}
+
+		if (delta<0) factor=1.25;
+		else factor=0.75;
+				
 		if (((viewbox.width*factor/image.width)>=0.05||(viewbox.height*factor/image.height)>=0.025) && 
 		((viewbox.width*factor/image.width)<=1||(viewbox.height*factor/image.height)<=1)) {
 			viewbox.width*=factor;
 			viewbox.height*=factor;
 			viewbox.x-=(viewbox.width-width)*0.5;
 			viewbox.y-=(viewbox.height-height)*0.5;
+			if (e!=null) {//line up map with mouse cursor
+				coords2=screen2svgCoords(e.clientX,e.clientY);//updated coords
+				dx=(coords.x-coords2.x);
+				dy=(coords.y-coords2.y);
+				viewbox.x=viewbox.x+dx;
+				viewbox.y=viewbox.y+dy;
+			}
 			map.setViewBox(viewbox.x, viewbox.y, viewbox.width, viewbox.height);
+		} else if ((viewbox.width*factor/image.width)>=0.05||(viewbox.height*factor/image.height)>=0.025) {
+			//fully zoomed out, but let's center the image at this point
+			reset(e);
 		}
+			
 	}
 	
+	/*svg.click(fun);
+	function fun(e){
+		centerX=viewbox.width/2+viewbox.x;
+		centerY=viewbox.height/2+viewbox.y;
+		
+		orginX1=viewbox.x;
+		orginY1=viewbox.y;
+
+		dx=(.75*viewbox.width-viewbox.width)/2;
+		dy=(.75*viewbox.height-viewbox.height)/2;
+		//alert(viewbox.x+","+viewbox.y+","+viewbox.width+","+viewbox.height);
+		
+		coords=screen2svgCoords(e.clientX,e.clientY);
+		
+		f=zoom(null,1);
+		
+		coords2=screen2svgCoords(e.clientX,e.clientY);
+		
+		//alert(coords.x-coords2.x);
+		
+		
+		orginX2=viewbox.x;
+		orginY2=viewbox.y;
+		
+		//alert(orginX1-orginX2-offsetX);
+		
+		//coords2=svg2screenCoords(coords.x,coords.y);	
+		//map.path("M" + coords.x + "," + coords.y + "L" + coords2.x + "," + coords2.y);
+		//x=map.circle(coords.x,coords.y,5);
+		//x.transform("T"+(coords.x-centerX)+","+(coords.y-centerY));
+		
+		dx=(coords.x-coords2.x);
+		dy=(coords.y-coords2.y);
+
+		
+		viewbox.x=viewbox.x+dx;
+		viewbox.y=viewbox.y+dy;
+		map.setViewBox(viewbox.x, viewbox.y, viewbox.width, viewbox.height);
+	}*/
+	
+	function zoomIn(e)  {
+		zoom(null,-1);
+	}
+	
+	function zoomOut(e) {
+		zoom(null,1);
+	}
+	
+	function center(e){
+		centerX=viewbox.width/2+viewbox.x;
+		centerY=viewbox.height/2+viewbox.y;
+		
+		coords=screen2svgCoords(e.clientX,e.clientY);
+		
+		//map.path("M" + centerX + "," + centerY + "L" + coords.x + "," + coords.y);
+		//x=map.circle(centerX,centerY,5);
+		//x.transform("T"+(coords.x-centerX)+","+(coords.y-centerY));
+		
+		dx=(coords.x-centerX);
+		dy=(coords.y-centerY);
+
+		
+		viewbox.x=viewbox.x+dx;
+		viewbox.y=viewbox.y+dy;
+		map.setViewBox(viewbox.x, viewbox.y, viewbox.width, viewbox.height);
+	}
+
+	
+	function screen2svgCoords(x,y) {
+		factor=viewbox.width/svg.width();
+		svgX=(x*factor)+viewbox.x;
+		svgY=(y*factor)+viewbox.y;
+		return {x:svgX,y:svgY};
+	}
+	
+	function svg2screenCoords(x,y) {
+		factor=viewbox.width/svg.width();
+		sX=(x-viewbox.x)/factor;
+		sY=(y-viewbox.y)/factor;
+		return {x:sX,y:sY};
+	}	
+
+	
 	function move (s) {
-		return function(dx, dy) {
+		return function (dx, dy) {
 			//(s||this).translate(dx-this.dx, dy-this.dy);
 			var x, y, s;
 			s=viewbox.width/image.width;
@@ -188,6 +305,7 @@ $(function() {
 			this.dy=dy;
 		}
 	}
+	
 	function movestart () {
 		this.dx=this.dy=0;
 	}
@@ -196,6 +314,7 @@ $(function() {
 		var scale;
 		if ($(window).width()>=$(window).height()) scale=$(window).width()/image.width;
 		else scale=$(window).height()/image.height;
+		scale*=.95; //Reduce scale to  95% of max to avoid scroll bars in some browsers
 		canvas.css({
 			'width': image.width*scale+'px',
 			'height': image.height*scale+'px',
@@ -207,10 +326,10 @@ $(function() {
 			'margin': '0 auto'
 		});
 		map.setSize(svg.width(), svg.height());
-	}
+	}	
 	
 	function reset(e) {
-		e.preventDefault();
+		if (e!=null) e.preventDefault();
 		var s=viewbox.width/image.width;
 		viewbox.x=viewbox.y=0;
 		viewbox.width/=s;
@@ -219,11 +338,14 @@ $(function() {
 	}
 	
 	function datachange(name, data) {
+		animate = $("#attributepane").is(":visible"); //only animate if the panel is shown
+		$("#attributepane").show();
+
 		$('#chartname').text(name);
 		var maxvalue=100;
 		var scale=plot.height/maxvalue;
 
-		values.attr('opacity', 0);
+		if (animate) values.attr('opacity', 0);
 		for (var i=0; i<values.length; i++) {
 			var t, ty;
 			if (data && !(isNaN(data[i]))) {
@@ -237,28 +359,39 @@ $(function() {
 			values[i].attr('text', t);
 			values[i].attr({y: ty});
 		}
-				
+		
+
 		for (var i=0; i<bars.length; i++) {
-			var h;
+			var h=0;
 			if (data && !(isNaN(data[i]))) {
 				h=data[i]*scale;
-			} else {
-				h=0*scale;
 			}
-			bars[i].animate({
-					height: h, 
-					y: plot.height-h}, 
-				500, 
-				'>',
-				function() {
-					values.animate({'opacity': 1}, 500);
-				}
-			);
+			
+			//animale only if the pane is visible
+			if( animate ) {
+				bars[i].animate({
+						height: h, 
+						y: plot.height-h}, 
+					500, 
+					'>',
+					function() {
+						values.animate({'opacity': 1}, 500);
+					}
+				);
+			} else {
+				bars[i].attr({
+						height: h, 
+						y: plot.height-h}
+				);
+			}
+			
 		}
 	}
+
 	
 	// initialize
-	$(window).resize();
+	//$(window).resize();
 	svg.animate({'opacity': 1}, 500);
-	  
+
+
 });
