fceaf0a4b92fa5c07a558f9587389e1d1668ebb2
computermacgyver <computermacgyver@gmail.com>
2012-07-31 21:01:01 +0100

    git-pages template mispelling

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,5 +4,5 @@ git clone git@github.com:oxfordinternetinstitute/InteractiveVis.git
 cd InteractiveVis
 git checkout --orphan gh-pages
 git add .
-git commit -a -m "auto-commit pages from master"
+git commit -a -m "gh-pages auto-commit pages from master"
 git push -f origin gh-pages
